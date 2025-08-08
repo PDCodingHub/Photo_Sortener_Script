320f4f77195be0fb0626b53757b397d6da3e5745
PDCodingHub <100873112+PDCodingHub@users.noreply.github.com>
2025-08-08 13:35:21 +0200

    Update name_formatter.sh
    - Se añade funcionalidad para que detecte también archivos de video y pueda renombrarlos de igual manera.
    
    - Se corrige error que convertía cualquier extensión a jpg.
    Ahora detecta la extensión del archivo, lo renombra y posteriormente vuelve a colocar la misma extensión.

--- name_formatter.sh
+++ name_formatter.sh
@@ -1,41 +1,58 @@
 #!/bin/bash
 
-# Directorio donde se ejecuta el script
 directorio=$(pwd)
 
-# Recorrer todas las imágenes en el directorio
-for imagen in "$directorio"/*.{jpg,jpeg,png,JPG,JPEG,PNG}; do
+for archivo in "$directorio"/*.{jpg,jpeg,png,mov,avi,mp4,JPG,JPEG,PNG,MOV,AVI,MP4}; do
+    [ -e "$archivo" ] || continue
 
-    # Verificar que el archivo exista (por si no hay coincidencias)
-    [ -e "$imagen" ] || continue
-
-    # Extraer la fecha de captura y reformatear para usarla en el nombre del archivo
-    fecha=$(exiftool -DateTimeOriginal -s3 "$imagen")
-	fecha_sola=$(echo "$fecha" | cut -d' ' -f1 | sed 's/://g')   
-	hora_sola=$(echo "$fecha" | cut -d' ' -f2 | sed 's/:/./g')     
-	fecha_captura="${fecha_sola}_${hora_sola}" 
-
-    # Si no se encuentra la fecha, saltar el archivo
-    [ -z "$fecha_captura" ] && echo "Sin fecha EXIF: $imagen" && continue
-
-# Base del nuevo nombre
-    base_nombre="IMG${fecha_captura}"
-    nuevo_nombre="${base_nombre}.jpg"
+	# Detecta la extensión original
+	extension="${archivo##*.}"
+	extension_lower=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
+	
+	# Detecta si es imagen o video
+	case "$extension_lower" in
+		jpg|jpeg|png)
+			base="IMG"
+			# Extraer la fecha de captura y reformatear para usarla en el nombre del archivo
+			fecha=$(exiftool -DateTimeOriginal -s3 "$archivo")
+			fecha_sola=$(echo "$fecha" | cut -d' ' -f1 | sed 's/://g')
+			hora_sola=$(echo "$fecha" | cut -d' ' -f2 | sed 's/:/./g')
+			fecha_captura="${fecha_sola}_${hora_sola}"
+	
+		[ -z "$fecha_captura" ] && echo "Sin fecha EXIF: $archivo" && continue
+			;;
+		mov|avi|mp4)
+			base="VID"
+			# Extraer la fecha de captura y reformatear para usarla en el nombre del archivo
+			fecha=$(exiftool -FileModifyDate -s3 "$archivo" | sed 's/+02.00//g')
+			fecha_sola=$(echo "$fecha" | cut -d' ' -f1 | sed 's/://g')   
+			hora_sola=$(echo "$fecha" | cut -d' ' -f2 | sed 's/:/./g')     
+			fecha_captura="${fecha_sola}_${hora_sola}" 
+			;;
+		*)
+			echo "Extensión no reconocida: $extension_lower"
+			continue
+			;;
+	esac
+	
+    base_nombre="${base}${fecha_captura}"
+    nuevo_nombre="${base_nombre}.${extension_lower}"
     nuevo_path="${directorio}/${nuevo_nombre}"
 
-    # Si ya existe, añadir sufijo incremental
+ 	# Si ya existe el nombre, le añade un contador para diferenciar la imagen y no sustituirla
     contador=2
     while [ -e "$nuevo_path" ]; do
-        nuevo_nombre="${base_nombre}_$contador.jpg"
+        nuevo_nombre="${base_nombre}_$contador.${extension_lower}"
         nuevo_path="${directorio}/${nuevo_nombre}"
         contador=$((contador + 1))
     done
-	
-# Renombrar el archivo
-    mv "$imagen" "$nuevo_path"
 
+	# Se renombra el archivo 
+    mv "$archivo" "$nuevo_path"
     echo "Archivo renombrado como: $nuevo_path"
 done
 
+echo ""
+echo "*******************************"
+echo "Se han renombrado los archivos"
 read -p "Presiona ENTER para salir..."
-
